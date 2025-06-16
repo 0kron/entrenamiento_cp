093f762866ef202edb2bb3dda6fac1447a507803
Ain Bolaños Cortés <88357657+0kron@users.noreply.github.com>
2025-06-16 10:31:29 -0600

    Add files via upload

--- codeforces/interestingdrink.cpp
+++ codeforces/interestingdrink.cpp
@@ -1,7 +1,17 @@
 // Interesting Drink
 #include <bits/stdc++.h>
 using namespace std; 
-main
+const int MAXN = 1e5 + 1; 
+int X[MAXN];  
+
 int main(){
+  int n, i, q, m; cin >> n; i = n;  
+  while (i--) cin >> X[i]; 
+  sort(X, X+n); 
+  cin >> q; 
+  while (q--){
+    cin >> m; 
+    cout << upper_bound(X, X+n, m)-X << endl; 
+  }
 
 }
